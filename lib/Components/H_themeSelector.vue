86530a115bf27afdf319f074b80e71c500f35563
Henrik Holte Larsen <lfhenrl@gmail.com>
2024-02-13 17:16:15 +0100

    mm

--- lib/Components/H_themeSelector.vue
+++ lib/Components/H_themeSelector.vue
@@ -1,35 +1,24 @@
 <template>
-  <div
-    class="H_themeSelector"
-    @click="click"
-    title="Change Theme: Light ot Dark"
-  >
-    <H_icon
-      icon="night"
-      btn="standard"
-      v-if="dark"
-      size="30px"
-      style="color: aqua"
-    />
-    <H_icon
-      icon="day"
-      btn="standard"
-      v-else="dark"
-      size="30px"
-      style="color: rgb(255, 119, 0)"
-    />
+  <div class="H_themeSelector" @click="click" title="Change Theme: Light ot Dark">
+    <H_icon icon="night" btn="standard" v-if="dark" size="30px" style="color: aqua" />
+    <H_icon icon="day" btn="standard" v-else="dark" size="30px" style="color: rgb(255, 119, 0)" />
   </div>
 </template>
 
 <script setup lang="ts">
 import H_icon from "./H_icon.vue";
 import { themeSelector } from "../utils/themeSelector";
-
+import { onMounted } from "vue";
+const E = defineEmits(["changed"]);
 const dark = themeSelector().darkTheme;
 
 function click() {
   dark.value = !dark.value;
+  E("changed", dark.value);
 }
+onMounted(() => {
+  E("changed", dark.value);
+});
 </script>
 <style>
 @layer hhl-components {
