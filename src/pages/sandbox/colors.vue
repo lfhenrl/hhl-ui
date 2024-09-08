fd2e10cd56e51556c900b2ae779ca099c6266088
Henrik Holte Larsen <lfhenrl@gmail.com>
2024-09-08 12:49:13 +0200

    npm

--- src/pages/sandbox/colors.vue
+++ src/pages/sandbox/colors.vue
@@ -56,6 +56,7 @@
     <div style="border: 1px solid gray; display: flex; align-items: center; margin-top: 20px">
       <H_datetime v-model="date" label="DATE"> </H_datetime>
     </div>
+    <div class="i-carbon-sun colRed"></div>
   </div>
 </template>
 
@@ -70,6 +71,22 @@ const date = ref();
 </script>
 
 <style>
+.colRed {
+  color: red;
+}
+@layer hhl-components {
+  .i-carbon-sun {
+    --un-icon: url("data:image/svg+xml;utf8,%3Csvg viewBox='0 0 32 32' width='1.2em' height='1.2em' xmlns='http://www.w3.org/2000/svg' %3E%3Cpath fill='currentColor' d='M16 12.005a4 4 0 1 1-4 4a4.005 4.005 0 0 1 4-4m0-2a6 6 0 1 0 6 6a6 6 0 0 0-6-6M5.394 6.813L6.81 5.399l3.505 3.506L8.9 10.319zM2 15.005h5v2H2zm3.394 10.193L8.9 21.692l1.414 1.414l-3.505 3.506zM15 25.005h2v5h-2zm6.687-1.9l1.414-1.414l3.506 3.506l-1.414 1.414zm3.313-8.1h5v2h-5zm-3.313-6.101l3.506-3.506l1.414 1.414l-3.506 3.506zM15 2.005h2v5h-2z'/%3E%3C/svg%3E");
+    -webkit-mask: var(--un-icon) no-repeat;
+    mask: var(--un-icon) no-repeat;
+    -webkit-mask-size: 100% 100%;
+    mask-size: 100% 100%;
+    background-color: currentColor;
+    color: inherit;
+    width: 1.2em;
+    height: 1.2em;
+  }
+}
 .colors {
   overflow: auto;
   --col-lightness: 60%;
