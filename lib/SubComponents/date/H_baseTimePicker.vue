9364da9e3a19e65dab4f1b906eddf16d9a1c37f7
Henrik Holte Larsen <lfhenrl@gmail.com>
2024-12-30 14:06:14 +0100

    timepicker

--- lib/SubComponents/date/H_baseTimePicker.vue
+++ lib/SubComponents/date/H_baseTimePicker.vue
@@ -25,6 +25,7 @@
         </div>
       </template>
       <div class="rounded bg-bg0 border-bg5 border">
+        {{ time }}
         <H_timeTable v-model="tempDato" :show-seconds="showSeconds" :isopen="popupOpen" />
         <div class="flex justify-end gap-4 p-4 border-t border-bg4">
           <H_btn size="sm" @click.stop="cancel" class="col-sec text-sm w-18">CANCEL</H_btn>
@@ -53,7 +54,7 @@ const emit = defineEmits(["timeChanged"]);
 
 const popupOpen = ref(false);
 const focused = ref(false);
-const tempDato = ref(props.time);
+const tempDato = computed(() => props.time);
 
 const getZeroInFront = (val: number) => (val < 10 ? "0" + val : val);
 const close = () => (popupOpen.value = false);
