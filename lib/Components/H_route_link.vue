2d9d207d8dae9e86f05b4f371f27d7e848f3c0c1
Henrik Holte Larsen <lfhenrl@gmail.com>
2025-04-16 14:23:09 +0200

    feat: update H_route_link component to use H_icon directly and refine icon prop type; update package dependencies

--- lib/Components/H_route_link.vue
+++ lib/Components/H_route_link.vue
@@ -2,17 +2,18 @@
   <a class="H_route_link" :href="to" :class="{ 'text-pri': isSelected }">
     <div class="col-start-1 row-start-1 row-end-3 h-full w-full" :class="{ 'bg-pri': isSelected }" />
     <div class="H_route_link_img flex items-center justify-center col-start-2 row-start-1">
-      <component :is="icon" />
+      <H_icon :name="icon as any" />
     </div>
     <div class="H_route_link_text flex items-center justify-center text-xs col-start-2 col-row-2">{{ title }}</div>
   </a>
 </template>
 
 <script setup lang="ts">
+import H_icon from "./H_icon.vue";
 defineProps({
   title: { type: String, default: "Title" },
   to: { type: String, default: "#/" },
-  icon: { type: Object },
+  icon: { type: String, default: "home" },
   disabled: { type: Boolean, default: false },
   isSelected: { type: Boolean, default: false },
 });
